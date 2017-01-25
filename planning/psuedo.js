a2e5eda27ed88f07611764c768c40c5e9eb3e655
Hannah King <hmking.nc@gmail.com>
2017-01-24 21:37:09 -0500

    pseudo added

---
+++ planning/psuedo.js
@@ -0,0 +1,29 @@
+//capture user inputs
+//ajax to brew API
+//	info we need to call
+	//address
+	//lat/long
+	//logo
+	//average rating
+	//#checkins
+	//name
+	//website
+	//social platforms
+	//hours
+	//...
+//append brewDiv to 'sidebar' with add button
+//	.on click of any brewDiv show hidden accordian div with more info from ajax call
+// .on click of addBtn push following info to array(s)
+//		json obj for brewery
+// 		ajax call to google api
+//			add pin to map
+//			update optimized waypoints
+
+//On Create my brewventure
+//	tripObj stored to FB
+//		trip information
+//		trip array
+//		trip ID
+
+//trip.html template
+//display previous trips
