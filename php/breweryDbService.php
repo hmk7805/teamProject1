9b47c284e88233a14021b4d500a327a58c2b5c5b
Jarrett Tolman <jtolman@Jarretts-MacBook-Pro.local>
2017-01-26 20:33:15 -0500

    firebase working, but commented out

--- php/breweryDbService.php
+++ php/breweryDbService.php
@@ -1,33 +1,5 @@
-<!-- 
-  require php/Pintlabs/Services/Brewerydb.php
-
-  use this to keep API key private
-
-  this service is provided by / listed on official website
-
--->
-
-<!--This service is a simple PHP wrapper for the Brewerydb.com API.
-
-
-Usage:
-
-$bdb = new Pintlabs_Service_Brewerydb($apikey);
-$bdb->setFormat('php'); // if you want to get php back.  'xml' and 'json' are also valid options.
-
-Then you can call the API:
-
-try {
-    // The first argument to request() is the endpoint you want to call
-    // 'brewery/BrvKTz', 'beers', etc.
-    // The third parameter is the HTTP method to use (GET, PUT, POST, or DELETE)
-    $results = $bdb->request('beers', $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
-} catch (Exception $e) {
-    $results = array('error' => $e->getMessage());
-}-->
-
-
 <?php
+    // https://github.com/PintLabs/PHP-Brewerydb
 
     require('Pintlabs/Service/Brewerydb.php');
 
@@ -36,7 +8,9 @@ try {
     $apikey = 'f0479b27c9f9d643b57d9bd53db8937f';
 
     $params = array(
-        "locality" => "charlotte",
+        "locality" => $_GET["locality"],
+        "locationType" => $_GET["locationType"],
+        "order" => $_GET["order"],
     );
 
     $bdb = new Pintlabs_Service_Brewerydb($apikey);
@@ -46,11 +20,11 @@ try {
         // The first argument to request() is the endpoint you want to call
         // 'brewery/BrvKTz', 'beers', etc.
         // The third parameter is the HTTP method to use (GET, PUT, POST, or DELETE)
-        $results = $bdb->request('beers', $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
+        $results = $bdb->request($_GET["endpoint"], $params, 'GET'); // where $params is a keyed array of parameters to send with the API call.
     } catch (Exception $e) {
         $results = array('error' => $e->getMessage());
     }
 
-    echo $results;
+    echo json_encode($results);
 
 ?>
