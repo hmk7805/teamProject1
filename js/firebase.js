0fd080a11f125f8e4136e0d8768c6b1d82871a10
Jarrett Tolman <jtolman@wifi_stu-10-38-70-21.uncc.edu>
2017-01-28 14:12:12 -0500

    adding firebase and trip js files

---
+++ js/firebase.js
@@ -0,0 +1,37 @@
+// Team Project 1 - BrewVentures
+// Jarrett Tolman - firebase.js
+
+var firebase = {
+    // function to add trip object to database
+
+    // function to get all trips from database
+
+    // function to get one trip from database
+}
+
+// <script src="https://www.gstatic.com/firebasejs/3.6.7/firebase.js"></script>
+
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyAqJwLe-xGxjo5IpHLcl28UNz7_5W7uEig",
+//     authDomain: "brewventures-bd06d.firebaseapp.com",
+//     databaseURL: "https://brewventures-bd06d.firebaseio.com",
+//     storageBucket: "brewventures-bd06d.appspot.com",
+//     messagingSenderId: "772130205350"
+// };
+// firebase.initializeApp(config);
+
+// // Create a variable to reference the database.
+// var database = firebase.database();
+
+// // Create a variable database reference object
+// var databaseRef = database.ref();
+
+// var tripsRef = database.ref("/trips");
+
+// // push to database
+// tripsRef.push({
+//     name: "test trip 01",
+//     date: "12345657686708",
+//     stops: ["NoDa Brewing", "Birdsong Brewery", "Blue Blaze Brewing"]
+// });
---
+++ js/trip.js
@@ -0,0 +1,7 @@
+// Team Project 1 - BrewVentures
+// Jarrett Tolman - trip.js
+
+var trip = {
+    // get trip from firebase
+    // populate html template
+}
