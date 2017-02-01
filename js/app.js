8dfc970487e4d4feedbaeac9946a022b7995a5f4
Rayal <rayalsgit@users.noreply.github.com>
2017-01-31 20:16:40 -0500

    modified logic for putting pin on map

--- js/app.js
+++ js/app.js
@@ -13,38 +13,46 @@ function accordion() {
             } else {
                 panel.style.maxHeight = panel.scrollHeight + "px";
             }
-            // addBrewery
-            addBrewery();
+
+            //// Grabs data from UI
+            var bName = $(this).data("name");
+            var lat = $(this).data("lat");
+            var lng = $(this).data("long");
+            // addBrewery icon
+            addBrewery(bName, lat, lng);
         }
     }
 };
 //accordian end
 
-// handler for new location entered by user
-$("#locationsubmit").on("click", function(event) {
-    // prevent event bubbling
-    event.preventDefault();
+// add brewery icon to map.
+function addBrewery(name, lat, lng) {
+    // Grabs input
+    var bName = name;
+    var lat = lat;
+    var lng = lng;
+    var loc = new google.maps.LatLng(lat, lng);
 
-    // clear the breweryList
-    breweryList = [];
+    // Creates local "temporary" object for holding brewery data
+    var newBrewery = {
+        name: bName,
+        loc: loc
+    };
 
-    // get the location from user
-    var loc = $("#locationform").val().trim();
-    // and draw a google map of the given location
-    placeEmptyMap(loc);
+    // add to brewery array
+    breweryList.push(newBrewery);
 
-    // get breweries
-    breweryAPI.setURL(name = loc);
-    breweryAPI.makeCall(accordion);
-});
+    // place a marker on the map 
+    placeMarker(map, loc, bName);
+};
 
 
-
-function addBrewery() {
+// add brewery icon to map.
+function addBrewery(name, lat, lng) {
     // Grabs user input
-    var bName = $(this).data("name");
-    var lat = $(this).data("lat");
-    var lng = $(this).data("long");
+    var bName = name;
+    var lat = lat;
+    var lng = lng;
     var loc = new google.maps.LatLng(lat, lng);
 
     // Creates local "temporary" object for holding brewery data
